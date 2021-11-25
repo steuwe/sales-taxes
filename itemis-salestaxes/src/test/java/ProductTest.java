7ddcb5e33f80620095df0302ffdeeea012662dd6
steuwe <52547093+steuwe@users.noreply.github.com>
2021-11-25 12:44:18 +0100

    create test for Product class

--- itemis-salestaxes/src/test/java/ProductTest.java
+++ itemis-salestaxes/src/test/java/ProductTest.java
@@ -1,4 +1,36 @@
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
 
 public class ProductTest {
 
+	
+	@Test
+	void testProductNameShouldNotThrow() {
+		Product product = new Product("box of chocolates", BigDecimal.valueOf(1));
+	}
+	
+	void testNullProductNameShouldThrowIllegalArgumentException() {
+		Product product = new Product(null, BigDecimal.valueOf(1));
+	}
+	
+	@Test
+	void testEmptyProductNameShouldThrowIllegalArgumentException() {
+		Product product = new Product("", BigDecimal.valueOf(1));
+	}
+	
+	@Test
+	void testProductPriceShouldNotThrow() {
+		Product product = new Product("box of chocolates", BigDecimal.valueOf(1.0));
+	}
+	
+	void testNullProductPriceShouldThrowIllegalArgumentException() {
+		Product product = new Product("box of chocolates", null);
+	}
+	
+	@Test
+	void testNegativeProductPriceShouldThrowIllegalArgumentException() {
+		Product product = new Product("", BigDecimal.valueOf(-1.0));
+	}
+	
 }
