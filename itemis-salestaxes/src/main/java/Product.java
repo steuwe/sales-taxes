121ba711e0943c8261a7bee340a95dd3c6dc5fa5
steuwe <52547093+steuwe@users.noreply.github.com>
2021-11-25 13:03:56 +0100

    Added basic name validation

--- itemis-salestaxes/src/main/java/Product.java
+++ itemis-salestaxes/src/main/java/Product.java
@@ -6,7 +6,22 @@ public class Product {
 	private BigDecimal price;
 	
 	public Product(String name, BigDecimal price) {
+		try {
+			validateName(name);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Product could not be created: " + e.getMessage());
+		}
 		
+	}
+	
+	private boolean validateName(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("Product name is null!");
+		}
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException("Procut name is empty!");
+		}
+		return true;
 	}
 	
 }
