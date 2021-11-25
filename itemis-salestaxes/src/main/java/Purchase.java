9f52744841794d2bd265e0d3808be10e54951190
steuwe <52547093+steuwe@users.noreply.github.com>
2021-11-25 17:34:36 +0100

    write tests for Purchase and implement Purchase functionality

--- itemis-salestaxes/src/main/java/Purchase.java
+++ itemis-salestaxes/src/main/java/Purchase.java
@@ -1,21 +1,119 @@
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Purchase {
 
+	private final BigDecimal SALES_TAX_PERCENTAGE = BigDecimal.valueOf(1);
+	private final BigDecimal IMPORT_TAX_PERCENTAGE = BigDecimal.valueOf(5);
 	private int quantity;
 	private boolean isExempt;
 	private boolean isImported;
 	private Product product;
-	
+
 	public Purchase(String input) {
-		
+		parseInputString(input);
 	}
-	
+
 	private void parseInputString(String input) {
-		
+		try {
+			validateInput(input);
+			String parsedInput = input.trim();
+			isImported = parseImported(parsedInput);
+			parsedInput = removeImportedFromInput(parsedInput);
+			quantity = parseQuantity(parsedInput);
+			parsedInput = removeQuantityFromInput(parsedInput);
+			product = parseProduct(parsedInput);
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Failed to create purchase: " + e.getMessage());
+		}
 	}
 	
-	private BigDecimal calculatePriceWithTaxes() {
-		return null;
+	private void validateInput(String input) {
+		if (input == null) {
+			throw new IllegalArgumentException("Input is null!");
+		}
+		if (input.isEmpty() || input.trim().isEmpty()) {
+			throw new IllegalArgumentException("Input is empty or blank!");
+		}
+		if (!input.contains("at")) {
+			throw new IllegalArgumentException("Input must indicate price with 'at'");
+		}
+	}
+	
+	private boolean parseImported(String input) {
+		if (input.contains("imported")) {
+			isImported = true;
+		}
+		return false;
+	}
+	
+	private String removeImportedFromInput(String input) {
+		if (isImported) {
+			return input.replace("imported ", "");
+		}
+		return input;
+	}
+	
+	private int parseQuantity(String input) {
+		String first_token = input.split(" ")[0];
+		try { 
+			int quantity = Integer.parseInt(first_token);
+			if (quantity < 1) {
+				throw new IllegalArgumentException("Product quantity is less than 1!");
+			}
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Product quantity is missing or not formatted correctly: " + e.getMessage());
+		}
+		return quantity;
+	}
+	
+	private String removeQuantityFromInput(String input) {
+		String quantityAsString = Integer.toString(quantity);
+		return input.replace(quantityAsString, "").trim();
+	}
+
+	private Product parseProduct(String input) {
+		String[] splitInput = input.split(" at ");
+		if (splitInput.length < 2) {
+			throw new IllegalArgumentException("Product name or price is missing!");
+		}
+		String productName = splitInput[0].trim();
+		try {
+			BigDecimal productPrice = new BigDecimal(splitInput[1].trim());
+			return new Product(productName, productPrice);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException("Product price is not formatted correctly: " + e.getMessage());
+		}
+	}
+	
+	public BigDecimal getPriceWithoutTaxes() {
+		return product.getPrice();
+	}
+
+	public BigDecimal getPriceWithTaxes() {
+		BigDecimal price = product.getPrice();
+		if (!isExempt) {
+			price = applySalesTax(price);
+			price = roundTaxedPrice(price);
+		}
+		price = applyImportTax(price);
+		price = roundTaxedPrice(price);
+		return price;
+	}
+	
+	private BigDecimal applySalesTax(BigDecimal price) {
+		return price.multiply(SALES_TAX_PERCENTAGE).divide(BigDecimal.valueOf(100));
+	}
+	
+	private BigDecimal applyImportTax(BigDecimal price) {
+		return price.multiply(IMPORT_TAX_PERCENTAGE).divide(BigDecimal.valueOf(100));
+	}
+	
+	private BigDecimal roundTaxedPrice(BigDecimal price) {
+		MathContext context = new MathContext(2);
+		price = price.multiply(BigDecimal.valueOf(20));
+		price = price.round(context);
+		price = price.divide(BigDecimal.valueOf(20));
+		return price;
 	}
 }
--- itemis-salestaxes/src/test/java/PurchaseTest.java
+++ itemis-salestaxes/src/test/java/PurchaseTest.java
@@ -1,4 +1,101 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PurchaseTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = { 
+			"1 book at 12.49", 
+			"1 imported box of chocolates at 10.00",
+			"1  imported  box  of  chocolates  at  10.00", 
+			"1 music cd at 12", 
+			"2 chocolate bars at 15.99",
+			"1 packet of headache pills at 9.75" })
+	void parseValidPurchaseShouldNotThrow(String input) {
+		Purchase purchase = new Purchase(input);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { 
+			"1  imported  box  of  chocolates  at  10.00", 
+			"1 music cd at 12",
+			"2 Chocolate bars at 15.99", 
+			"2 Chocolate bars at 05.09" })
+	void parsePurchaseFringeCasesShouldNotThrow(String input) {
+		Purchase purchase = new Purchase(input);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { 
+			"1 imported box of chocolates at -10.00", 
+			"1 music cd at -12", 
+			"2 Chocolate bars at " })
+	void parseInvalidPurchasePriceShouldThrowIllegalArgumentException(String input) {
+		Purchase purchase = new Purchase(input);
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = { 
+			"1  imported at 10.00", 
+			"1 at 12.00", 
+			"1 music at 12.00" })
+	void parseInvalidPurchaseProductShouldThrowIllegalArgumentException(String input) {
+		Purchase purchase = new Purchase(input);
+	}
+
+	@Test
+	void getPriceForBookShouldNotThrow() {
+		Purchase purchase = new Purchase("1 book at 12.49");
+		BigDecimal actual_price = purchase.getPriceWithTaxes();
+		BigDecimal expected_price = BigDecimal.valueOf(12.49);
+		assertTrue(actual_price.compareTo(expected_price) == 0);
+	}
+
+	@Test
+	void getPriceForFoodShouldNotThrow() {
+		Purchase purchase = new Purchase("1 chocolate bar at 0.85");
+		BigDecimal actual_price = purchase.getPriceWithTaxes();
+		BigDecimal expected_price = BigDecimal.valueOf(0.85);
+		assertTrue(actual_price.compareTo(expected_price) == 0);
+	}
+	
+	@Test
+	void getPriceForMedicalProductShouldNotThrow() {
+		Purchase purchase = new Purchase("1 packet of headache pills at 9.75");
+		BigDecimal actual_price = purchase.getPriceWithTaxes();
+		BigDecimal expected_price = BigDecimal.valueOf(9.75);
+		assertTrue(actual_price.compareTo(expected_price) == 0);
+	}
+
+	@Test
+	void calculateSalesTaxForNonExemptProductShouldNotThrow() {
+		Purchase purchase = new Purchase("1 music cd at 14.99");
+		BigDecimal actual_price = purchase.getPriceWithTaxes();
+		BigDecimal expected_price = BigDecimal.valueOf(16.49);
+		assertTrue(actual_price.compareTo(expected_price) == 0);
+	}
+	
+	@Test
+	void calculateTaxForImportedNonExemptProductShouldNotThrow() {
+		Purchase purchase = new Purchase("1 imported bottle of perfume at 27.99");
+		BigDecimal actual_price = purchase.getPriceWithTaxes();
+		BigDecimal expected_price = BigDecimal.valueOf(32.19);
+		assertTrue(actual_price.compareTo(expected_price) == 0);
+	}
+	
+	@Test
+	void calculateTaxForImportedExemptProductShouldNotThrow() {
+		Purchase purchase = new Purchase("1 imported box of chocolates at 10.00");
+		BigDecimal actual_price = purchase.getPriceWithTaxes();
+		BigDecimal expected_price = BigDecimal.valueOf(10.50);
+		assertTrue(actual_price.compareTo(expected_price) == 0);
+	}
+
+	
 
 }
