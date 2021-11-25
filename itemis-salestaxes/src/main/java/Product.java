c0fa453b14d97a05981bcab78fca544b6d7f0dec
steuwe <52547093+steuwe@users.noreply.github.com>
2021-11-25 13:08:29 +0100

    Added basic price validation. Product tests now pass

--- itemis-salestaxes/src/main/java/Product.java
+++ itemis-salestaxes/src/main/java/Product.java
@@ -8,20 +8,31 @@ public class Product {
 	public Product(String name, BigDecimal price) {
 		try {
 			validateName(name);
+			validatePrice(price);
 		} catch (IllegalArgumentException e) {
 			throw new IllegalArgumentException("Product could not be created: " + e.getMessage());
 		}
+		this.name = name;
+		this.price = price;
 		
 	}
 	
-	private boolean validateName(String name) {
+	private void validateName(String name) {
 		if (name == null) {
 			throw new IllegalArgumentException("Product name is null!");
 		}
 		if (name.isEmpty()) {
 			throw new IllegalArgumentException("Procut name is empty!");
 		}
-		return true;
+	}
+	
+	private void validatePrice(BigDecimal price) {
+		if (price == null) {
+			throw new IllegalArgumentException("Product price is null");
+		}
+		if (price.compareTo(BigDecimal.valueOf(0)) < 0) {
+			throw new IllegalArgumentException("Product price is negative!");
+		}
 	}
 	
 }
--- itemis-salestaxes/src/test/java/ProductTest.java
+++ itemis-salestaxes/src/test/java/ProductTest.java
@@ -2,41 +2,40 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ProductTest {
+	
+	private BigDecimal defaultValidPrice = BigDecimal.valueOf(1);
+	private String defaultValidName = "box of chocolates";
 
 	
 	@Test
-	void testProductNameShouldNotThrow() {
-		Product product = new Product("box of chocolates", BigDecimal.valueOf(1));
+	void testProductNameAndPriceShouldNotThrow() {
+		Product product = new Product(defaultValidName, defaultValidPrice);
 	}
 	
 	void testNullProductNameShouldThrowIllegalArgumentException() {
 		assertThrows(IllegalArgumentException.class, 
-				() -> new Product(null, BigDecimal.valueOf(1)));
+				() -> new Product(null, defaultValidPrice));
 	}
 	
 	@Test
 	void testEmptyProductNameShouldThrowIllegalArgumentException() {
 		assertThrows(IllegalArgumentException.class, 
-				() -> new Product("", BigDecimal.valueOf(1)));
-	}
-	
-	@Test
-	void testProductPriceShouldNotThrow() {
-		Product product = new Product("box of chocolates", BigDecimal.valueOf(1.0));
+				() -> new Product("", defaultValidPrice));
 	}
 	
 	void testNullProductPriceShouldThrowIllegalArgumentException() {
 		assertThrows(IllegalArgumentException.class, 
-				() -> new Product("box of chocolates", null));
+				() -> new Product(defaultValidName, null));
 	}
 	
 	@Test
 	void testNegativeProductPriceShouldThrowIllegalArgumentException() {
 		assertThrows(IllegalArgumentException.class, 
-				() -> new Product("", BigDecimal.valueOf(-1.0)));
+				() -> new Product(defaultValidName, BigDecimal.valueOf(-1.0)));
 	}
 	
 }
