07d401e9528b9e6fbff6a80dc0a23aa4d5e0eac2
steuwe <52547093+steuwe@users.noreply.github.com>
2021-11-26 09:35:19 +0100

    write tests for and implement ShoppingBasket class. All tests pass

--- itemis-salestaxes/src/main/java/ShoppingBasket.java
+++ itemis-salestaxes/src/main/java/ShoppingBasket.java
@@ -1,15 +1,70 @@
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ShoppingBasket {
 
 	private List<Purchase> purchases;
 
-	public List<Purchase> getPurchases() {
-		return purchases;
+	public ShoppingBasket() {
+		purchases = new LinkedList<Purchase>();
 	}
 	
-	public void addPurchase(Purchase item) {
-		
+	public void addPurchase(String item) {
+		try {
+			Purchase purchase = new Purchase(item);
+			purchases.add(purchase);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Illegal input: ");
+			System.out.println(e.getMessage());
+			System.out.println("Try again");
+		}
+	}
+	
+	public String getReceipt() {
+		DecimalFormat df = new DecimalFormat("0.00");
+		StringBuffer receipt = new StringBuffer("");
+		BigDecimal totalSalesTax = BigDecimal.valueOf(0);
+		BigDecimal totalPrice = BigDecimal.valueOf(0);
+		for (Purchase purchase : purchases) {
+			totalSalesTax = totalSalesTax.add(purchase.getAmountTaxedBySalesTax());
+			StringBuffer purchaseDetails = new StringBuffer("");
+			purchaseDetails.append(purchase.getQuantity() + " ");
+			if (purchase.isImported()) {
+				purchaseDetails.append("imported ");
+			}
+			purchaseDetails.append(purchase.getProductName());
+			purchaseDetails.append(": ");
+			purchaseDetails.append(df.format(purchase.getPriceWithTaxes()));
+			totalPrice = totalPrice.add(purchase.getPriceWithTaxes());
+			purchaseDetails.append("\n");
+			receipt.append(purchaseDetails);
+		}
+		receipt.append("Sales Taxes: " + df.format(totalSalesTax));
+		receipt.append("\n");
+		receipt.append("Total: " + df.format(totalPrice));
+		return receipt.toString();
+	}
+	
+	public BigDecimal getTotalPrice() {
+		BigDecimal totalPrice = BigDecimal.valueOf(0);
+		for (Purchase purchase : purchases) {
+			totalPrice = totalPrice.add(purchase.getPriceWithTaxes());
+		}
+		return totalPrice;
+	}
+	
+	public BigDecimal getTotalSalesTax() {
+		BigDecimal totalTax = BigDecimal.valueOf(0);
+		for (Purchase purchase : purchases) {
+			totalTax = totalTax.add(purchase.getAmountTaxedBySalesTax());
+		}
+		return totalTax;
+	}
+	
+	public List<Purchase> getPurchases() {
+		return purchases;
 	}
 	
 }
--- itemis-salestaxes/src/test/java/ShoppingBasketTest.java
+++ itemis-salestaxes/src/test/java/ShoppingBasketTest.java
@@ -1,4 +1,86 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ShoppingBasketTest {
-
+	
+	private ShoppingBasket basket;
+	
+	@BeforeEach
+	void createBasket() {
+		basket = new ShoppingBasket();
+	}
+	
+	@Test
+	void testGetTotalPrice() {
+		basket.addPurchase("1 book at 12.49");
+		basket.addPurchase("1 music CD at 14.99");
+		basket.addPurchase("1 chocolate bar at 0.85");
+		BigDecimal totalPrice = basket.getTotalPrice();
+		BigDecimal expectedPrice = BigDecimal.valueOf(29.83);
+		assertTrue(totalPrice.compareTo(expectedPrice) == 0);
+		}
+	
+	@Test
+	void testGetTotalTax() {
+		basket.addPurchase("1 book at 12.49");
+		basket.addPurchase("1 music CD at 14.99");
+		basket.addPurchase("1 chocolate bar at 0.85");
+		BigDecimal totalTax= basket.getTotalSalesTax();
+		BigDecimal expectedTax = BigDecimal.valueOf(1.5);
+		assertTrue(totalTax.compareTo(expectedTax) == 0);
+		}
+	
+	@Test
+	void printReceiptDetails() {
+		basket.addPurchase("1 book at 12.49");
+		basket.addPurchase("1 music CD at 14.99");
+		basket.addPurchase("1 chocolate bar at 0.85");
+		String receipt = basket.getReceipt();
+		String expected = 
+				"""
+				1 book: 12.49
+				1 music CD: 16.49
+				1 chocolate bar: 0.85
+				Sales Taxes: 1.50
+				Total: 29.83""";
+		assertEquals(expected, receipt);
+	}
+	
+	@Test
+	void printReceiptDetails2() {
+		basket.addPurchase("1 imported box of chocolates at 10.00");
+		basket.addPurchase("1 imported bottle of perfume at 47.50");
+		String receipt = basket.getReceipt();
+		String expected = 
+				"""
+				1 imported box of chocolates: 10.50
+				1 imported bottle of perfume: 54.65
+				Sales Taxes: 7.65
+				Total: 65.15""";
+		assertEquals(expected, receipt);
+	}
+	
+	@Test
+	void printReceiptDetails3() {
+		basket.addPurchase("1 imported bottle of perfume at 27.99");
+		basket.addPurchase("1 bottle of perfume at 18.99");
+		basket.addPurchase("1 packet of headache pills at 9.75");
+		basket.addPurchase("1 box of imported chocolates at 11.25");
+		String receipt = basket.getReceipt();
+		String expected = 
+				"""
+				1 imported bottle of perfume: 32.19
+				1 bottle of perfume: 20.89
+				1 packet of headache pills: 9.75
+				1 imported box of chocolates: 11.85
+				Sales Taxes: 6.70
+				Total: 74.68""";
+		assertEquals(expected, receipt);
+	}
+	
 }
