e1dec43cfd53e99b0ee3d5e8e8935895c03172ff
steuwe <52547093+steuwe@users.noreply.github.com>
2021-11-30 14:16:16 +0100

    delete unused import

--- itemis-salestaxes/src/test/java/ProductTest.java
+++ itemis-salestaxes/src/test/java/ProductTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.math.BigDecimal;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
